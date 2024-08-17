52cf9b46aca3e420539c43a84bb3e33b6641c77b
haafizharafs <haafizharafs@gmail.com>
2024-08-17 11:02:35 +0700

    ubah versi di footer

--- resources/views/layouts/footers/footer.blade.php
+++ resources/views/layouts/footers/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="d-flex align-items-center justify-content-between">
             <div class="">
                 <div class="copyright text-center text-sm text-muted text-lg-start">
-                    Big Store v0.0.1a
+                    Big Store v0.1beta
                     © PT. Borneo Inovasi Gemilang
                 </div>
             </div>
